2c1437d517c40780e5f2f251cbfa236d100a0a31
gamma2653 <christopher2653@gmail.com>
2020-02-14 14:59:29 -0500

    More compliments

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,31 +1,35 @@
 sentences = [
 	[
-		"You make me feel {adj}.",
-		"Everytime I see you, I feel {adj}.",
-		"I’m glad you’re here. It makes me {adj}.",
-		"You actually using this site? This is why you make me {adj}."
+		"When you are around, I {adj}. <3",
+		"Everytime I see you, I {adj}. :3",
+		"I’m glad you’re here. I {adj}.",
+		"You are actually using this site? This is why I {adj}."
 	],
 	[
-		"You are {adj}.",
-		"I always knew you were {adj}.",
+		"You are {adj}. Seriously. Just thought you should know.",
+		"I always knew you were {adj}. ^_^",
 		"Did I ever tell you I always thought you were {adj}?",
-		"Your cookies are {adj}!",
-		"Your eyes are {adj}.",
-		"Your face is {adj}.",
-		"Just you entering the room is {adj}"
+		"Your cookies showed me how {adj} you are!",
+		"Your eyes are {adj}. I always find myself thinking of them.",
+		"Your face is {adj}, and absolutely adorable!",
+		"Just you entering the room is {adj}. Too much? Not really. You honestly are that awesome."
 	]
 ]
 
 adjs = [
 	[
-		"happy",
-		"great",
-		"nice",
-		"delighted",
-		"overjoyed",
-		"at peace"
+		"am happy",
+		"feel wonderful",
+		"feel nice",
+		"am overjoyed",
+		"am at peace",
+		"feel like my day brightend up",
+		"feel butterflies in my stomach",
+		"feel like I can take anything on",
+		"feel myself falling in love"
 	],
 	[
+		"delightful",
 		"amazing",
 		"awesome",
 		"lovely",
@@ -33,7 +37,8 @@ adjs = [
 		"loving",
 		"fantastic",
 		"pretty",
-		"graceful"
+		"graceful",
+		"warm"
 	]
 ]
 
